a55037f46517379f17a80e701c2efaeede0f7785
bhagyaj <bhagyajkumar1@gmail.com>
2025-03-15 22:30:51 +0530

    added cross origin middleware

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 import numpy as np
@@ -24,6 +25,20 @@ class ChatOutputModel(BaseModel):
     output:str
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Download NLTK resources
